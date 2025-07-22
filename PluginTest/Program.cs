85778e9109a19e255b9ad35db043ffe67a2ac00a
Çağatay <cagatay.alptekin038@gmail.com>
2025-07-22 11:14:10 +0300

    port

--- PluginTest/Program.cs
+++ PluginTest/Program.cs
@@ -13,6 +13,8 @@ namespace PluginTest
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            builder.WebHost.UseUrls($"http://*:{port}");
 
             var app = builder.Build();
 
