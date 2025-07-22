b9bf5dcc63b8fdc1c89ffe15e9efba3b23cbe2f4
Çağatay <cagatay.alptekin038@gmail.com>
2025-07-22 10:48:11 +0300

    docker

--- PluginTest/obj/Debug/net8.0/PluginTest.AssemblyInfo.cs
+++ PluginTest/obj/Debug/net8.0/PluginTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fef31adf7f8b2ffd10f1d89776ca16b897c111")]
 [assembly: System.Reflection.AssemblyProductAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
