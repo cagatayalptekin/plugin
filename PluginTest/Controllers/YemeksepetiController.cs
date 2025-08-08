043e1cccd5a58914021a0dfc7f2599ec89fa57d2
Çağatay <cagatay.alptekin038@gmail.com>
2025-08-08 13:06:41 +0300

    testlog7

--- PluginTest/Controllers/YemeksepetiController.cs
+++ PluginTest/Controllers/YemeksepetiController.cs
@@ -141,181 +141,181 @@ namespace PluginTest.Controllers
     public class YemeksepetiOrderModel
     {
         // Required fields
-        public string token { get; set; }
-        public string code { get; set; }
-        public Comments comments { get; set; }
-        public DateTime createdAt { get; set; }
-        public Customer customer { get; set; }
-        public List<Discount> discounts { get; set; }
-        public string expeditionType { get; set; }
-        public DateTime expiryDate { get; set; }
-        public LocalInfo localInfo { get; set; }
-        public Payment payment { get; set; }
-        public bool test { get; set; }
-        public bool preOrder { get; set; }
-        public PlatformRestaurant platformRestaurant { get; set; }
-        public Price price { get; set; }
-        public List<Product> products { get; set; }
-        public string corporateTaxId { get; set; }
-        public CallbackUrls callbackUrls { get; set; }
+        public string token { get; set; } = String.Empty;
+        public string code { get; set; } = String.Empty;
+        public Comments? comments { get; set; }
+        public DateTime? createdAt { get; set; }
+        public Customer? customer { get; set; }
+        public List<Discount>? discounts { get; set; }
+        public string expeditionType { get; set; } = String.Empty;
+        public DateTime? expiryDate { get; set; }
+        public LocalInfo? localInfo { get; set; }
+        public Payment? payment { get; set; }
+        public bool? test { get; set; }
+        public bool? preOrder { get; set; }
+        public PlatformRestaurant? platformRestaurant { get; set; }
+        public Price? price { get; set; }
+        public List<Product>? products { get; set; }
+        public string? corporateTaxId { get; set; }
+        public CallbackUrls? callbackUrls { get; set; }
 
         // Optional fields
-        public Delivery delivery { get; set; }
-        public ExtraParameters extraParameters { get; set; }
-        public InvoicingInformation invoicingInformation { get; set; }
-        public string shortCode { get; set; }
-        public Pickup pickup { get; set; }
-        public PreparationTimeAdjustments preparationTimeAdjustments { get; set; }
+        public Delivery? delivery { get; set; }
+        public ExtraParameters? extraParameters { get; set; }
+        public InvoicingInformation? invoicingInformation { get; set; }
+        public string? shortCode { get; set; }
+        public Pickup? pickup { get; set; }
+        public PreparationTimeAdjustments? preparationTimeAdjustments { get; set; }
     }
 
     public class Comments
     {
-        public string customerComment { get; set; }
+        public string? customerComment { get; set; }
     }
 
     public class Customer
     {
-        public string email { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string mobilePhone { get; set; }
-        public List<string> flags { get; set; }
+        public string? email { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? mobilePhone { get; set; }
+        public List<string>? flags { get; set; }
     }
 
     public class Delivery
     {
-        public Address address { get; set; }
-        public DateTime expectedDeliveryTime { get; set; }
-        public bool expressDelivery { get; set; }
+        public Address? address { get; set; }
+        public DateTime? expectedDeliveryTime { get; set; }
+        public bool? expressDelivery { get; set; }
         public DateTime? riderPickupTime { get; set; }
     }
 
     public class Address
     {
-        public string postcode { get; set; }
-        public string city { get; set; }
-        public string street { get; set; }
-        public string number { get; set; }
+        public string? postcode { get; set; }
+        public string? city { get; set; }
+        public string? street { get; set; }
+        public string? number { get; set; }
     }
 
     public class Discount
     {
-        public string name { get; set; }
-        public string amount { get; set; }
-        public List<Sponsorship> sponsorships { get; set; }
+        public string? name { get; set; }
+        public string? amount { get; set; }
+        public List<Sponsorship>? sponsorships { get; set; }
     }
 
     public class Sponsorship
     {
-        public string sponsor { get; set; }
-        public string amount { get; set; }
+        public string? sponsor { get; set; }
+        public string? amount { get; set; }
     }
 
     public class ExtraParameters
     {
-        public Dictionary<string, string> properties { get; set; }
+        public Dictionary<string, string>? properties { get; set; }
     }
 
     public class InvoicingInformation
     {
-        public string carrierType { get; set; }
-        public string carrierValue { get; set; }
+        public string? carrierType { get; set; }
+        public string? carrierValue { get; set; }
     }
 
     public class LocalInfo
     {
-        public string countryCode { get; set; }
-        public string currencySymbol { get; set; }
-        public string platform { get; set; }
-        public string platformKey { get; set; }
+        public string? countryCode { get; set; }
+        public string? currencySymbol { get; set; }
+        public string? platform { get; set; }
+        public string? platformKey { get; set; }
     }
 
     public class Payment
     {
-        public string status { get; set; }
-        public string type { get; set; }
+        public string? status { get; set; }
+        public string? type { get; set; }
     }
 
     public class Pickup
     {
         public DateTime? pickupTime { get; set; }
-        public string pickupCode { get; set; }
+        public string? pickupCode { get; set; }
     }
 
     public class PlatformRestaurant
     {
-        public string id { get; set; }
+        public string? id { get; set; }
     }
 
     public class Price
     {
         // Required fields
-        public List<DeliveryFee> deliveryFees { get; set; }
-        public string grandTotal { get; set; }
-        public string totalNet { get; set; }
-        public string vatTotal { get; set; }
+        public List<DeliveryFee>? deliveryFees { get; set; }
+        public string? grandTotal { get; set; }
+        public string? totalNet { get; set; }
+        public string? vatTotal { get; set; }
 
         // Optional fields
-        public string payRestaurant { get; set; }
-        public string riderTip { get; set; }
-        public string collectFromCustomer { get; set; }
+        public string? payRestaurant { get; set; }
+        public string? riderTip { get; set; }
+        public string? collectFromCustomer { get; set; }
     }
 
     public class DeliveryFee
     {
-        public string name { get; set; }
-        public double value { get; set; }
+        public string? name { get; set; }
+        public double? value { get; set; }
     }
 
     public class Product
     {
-        public string categoryName { get; set; }
-        public string name { get; set; }
-        public string paidPrice { get; set; }
-        public string quantity { get; set; }
-        public string remoteCode { get; set; }
-        public List<SelectedTopping> selectedToppings { get; set; }
-        public string unitPrice { get; set; }
-        public string comment { get; set; }
-        public string id { get; set; }
-        public string itemUnavailabilityHandling { get; set; }
-        public Variation variation { get; set; }
-        public List<Discount> discounts { get; set; }
+        public string? categoryName { get; set; }
+        public string? name { get; set; }
+        public string? paidPrice { get; set; }
+        public string? quantity { get; set; }
+        public string? remoteCode { get; set; }
+        public List<SelectedTopping>? selectedToppings { get; set; }
+        public string? unitPrice { get; set; }
+        public string? comment { get; set; }
+        public string? id { get; set; }
+        public string? itemUnavailabilityHandling { get; set; }
+        public Variation? variation { get; set; }
+        public List<Discount>? discounts { get; set; }
     }
 
     public class SelectedTopping
     {
-        public List<object> children { get; set; }
-        public string name { get; set; }
-        public string price { get; set; }
-        public int quantity { get; set; }
-        public string id { get; set; }
-        public string remoteCode { get; set; }
-        public string type { get; set; }
-        public string itemUnavailabilityHandling { get; set; }
-        public List<Discount> discounts { get; set; }
+        public List<object>? children { get; set; }
+        public string? name { get; set; }
+        public string? price { get; set; }
+        public int? quantity { get; set; }
+        public string? id { get; set; }
+        public string? remoteCode { get; set; }
+        public string? type { get; set; }
+        public string? itemUnavailabilityHandling { get; set; }
+        public List<Discount>?   discounts { get; set; }
     }
 
     public class Variation
     {
-        public string name { get; set; }
+        public string? name { get; set; }
     }
 
     public class PreparationTimeAdjustments
     {
-        public DateTime maxPickUpTimestamp { get; set; }
-        public DateTime minPickupTimestamp { get; set; }
-        public List<int> preparationTimeChangeIntervalsInMinutes { get; set; }
+        public DateTime? maxPickUpTimestamp { get; set; }
+        public DateTime? minPickupTimestamp { get; set; }
+        public List<int>? preparationTimeChangeIntervalsInMinutes { get; set; }
     }
 
     public class CallbackUrls
     {
-        public string orderAcceptedUrl { get; set; }
-        public string orderRejectedUrl { get; set; }
-        public string orderPickedUpUrl { get; set; }
-        public string orderPreparedUrl { get; set; }
-        public string orderProductModificationUrl { get; set; }
-        public string orderPreparationTimeAdjustmentUrl { get; set; }
+        public string? orderAcceptedUrl { get; set; }
+        public string? orderRejectedUrl { get; set; }
+        public string? orderPickedUpUrl { get; set; }
+        public string? orderPreparedUrl { get; set; }
+        public string? orderProductModificationUrl { get; set; }
+        public string? orderPreparationTimeAdjustmentUrl { get; set; }
     }
 
 
