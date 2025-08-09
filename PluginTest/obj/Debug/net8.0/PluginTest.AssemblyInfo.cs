54f612bdfe6cfa1e2c12e5adacb114b5fa4b690b
Çağatay <cagatay.alptekin038@gmail.com>
2025-08-09 22:49:31 +0300

    oneproject

--- PluginTest/obj/Debug/net8.0/PluginTest.AssemblyInfo.cs
+++ PluginTest/obj/Debug/net8.0/PluginTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6025c89d781ea77d1a6eb036044fef7963569ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39984ba425a69d15d1cca9e27556f13539198b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
