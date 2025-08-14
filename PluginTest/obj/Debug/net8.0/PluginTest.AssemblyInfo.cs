fe307b2ae44b22ad12b83b70daa1bd097ada1826
Çağatay <cagatay.alptekin038@gmail.com>
2025-08-14 13:20:57 +0300

    change-getir

--- PluginTest/obj/Debug/net8.0/PluginTest.AssemblyInfo.cs
+++ PluginTest/obj/Debug/net8.0/PluginTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a97fdb354adfbd49494d0058fcb939b2267a29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd19b2d817e615cb1359620580dd458b1f1dc356")]
 [assembly: System.Reflection.AssemblyProductAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
