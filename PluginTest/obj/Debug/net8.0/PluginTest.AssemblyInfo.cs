e98cbe8a293f3eed933a5fbdf7e8f2ed8e3a7160
Çağatay <cagatay.alptekin038@gmail.com>
2025-09-10 17:15:58 +0300

    yemeksepeti-map2

--- PluginTest/obj/Debug/net8.0/PluginTest.AssemblyInfo.cs
+++ PluginTest/obj/Debug/net8.0/PluginTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa636544e7735a9d7e51abf15f182a031b4d8d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a094793145b01b8103be623c08cb556762a10f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PluginTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
